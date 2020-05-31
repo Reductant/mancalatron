62425ff55e0d5d67742fa1a2db1ba019d2f2d7c4
Adam Wilson <adamcwilson@live.co.uk>
2020-05-31 15:19:03 +0100

    1/2-move lookahead fully functional

--- mancalatron_v3.cpp
+++ mancalatron_v3.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 #include <vector>
-#include "mancalatron_v2.h"
+#include "mancalatron_v3.h"
+
+
+std::vector<int> predict_move_outcomes(game_state game_as_is) {
+
+  // Initialise a post-move game_state
+  struct game_state potential_game_state;
+
+  // To hold predicted scores output by the score_predict function
+  std::vector<int> predicted_score = {0, 0, 0, 0, 0, 0};
+
+  // Get well and check that it's between 1 and 6 and nonempty
+  for (int i = 0; i < 6; ++i) {
+    if (game_as_is.board[i + 1] == 0) {
+      predicted_score[i] = -1;    // -1 as a flag for INVALID WELL
+    } else {
+      // Implement the effects of this well choice
+      potential_game_state = update_game_state(game_as_is, i + 1);
+      predicted_score[i] = potential_game_state.board[7];
+    }
+  }
+
+  return(predicted_score);
+}
 
 
 int main() {
@@ -56,34 +79,32 @@ int main() {
 
     // AI makes a move, with 1/2-step lookahead
     {
+
+      std::vector<int> predicted_score;
+
       // Initialise a post-move game_state
       struct game_state potential_game_state;
 
-      // To hold predicted scores output by the score_predict function
-      std::vector<int> predicted_score = {0, 0, 0, 0, 0, 0};
 
-      // Get well and check that it's between 1 and 6 and nonempty
-      for (int i = 0; i < 6; ++i) {
-        if (game_as_is.board[i + 1] == 0) {
-          predicted_score[i] = -1;    // -1 as a flag for INVALID WELL
-        } else {
-          // Implement the effects of this well choice
-          potential_game_state = update_game_state(game_as_is, i + 1);
-          predicted_score[i] = potential_game_state.board[7];
+      predicted_score = predict_move_outcomes(game_as_is);
+
+      int current_highest_score = -1;
+      int current_best_well = 1;
+      for(int i = 0; i < 6; ++i) {
+        if (predicted_score[i] > current_highest_score) {
+          current_highest_score = predicted_score[i];
+          current_best_well = i + 1;
         }
-
-
       }
+
+      potential_game_state = update_game_state(game_as_is, current_best_well);
 
       // The post-move game state becomes the new as_is game state
       game_as_is = potential_game_state;
 
-      std::cout << "\n\n";
-      for (int i = 0; i < 6; ++i) std::cout << predicted_score[i] << " ";
-      std::cout << "\n\n";
+      std::cout << "\n\nAI chooses well " << current_best_well << "\n\n";
 
     }
-
 
     // Rotate the board for the next user
     game_as_is.board = flip_board(game_as_is.board);
